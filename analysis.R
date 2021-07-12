ecd36401dfcb02ae99b55b098d80e0bab109f125
Heather Kropp <kroppheather@gmail.com>
2021-07-12 12:20:03 -0400

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -11,4 +11,20 @@ siteID <- read.csv(paste0(DIR,"/siteinfo.csv"))
 vegeClass <- read.csv(paste0(DIR,"/vege_class.csv"))
 vegeID <- read.csv(paste0(DIR,"/vegeID.csv"))
 
+#check that site 44 is not included
+soil <- soil[soil$site_id != 44,]
+air <- air[air$site_id != 44,]
+#note looks like I actually removed it before
+#adding to ADC
 
+#subset for winter period
+
+
+
+#start with using water year. May need to see if freezing starts in sept
+#start with soil
+
+#get water year
+soil$leapID <- leap_year(soil$year_st)
+soil$wyear <- ifelse( soil$leapID == TRUE & soil$doy_st >= 275, soil$year_st+1,
+                      ifelse(soil$leapID == FALSE & soil$doy_st >= 274,soil$year_st+1,soil$year_st))
